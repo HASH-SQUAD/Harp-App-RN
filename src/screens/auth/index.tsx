9ae79110d670914fe01ac430dedf25e89ecd21bf
Jamkris <dltmdgus1412@gmail.com>
2024-09-05 17:08:39 +0900

    CHORE :: Stack - Auth

--- src/screens/auth/index.tsx
+++ src/screens/auth/index.tsx
@@ -1,14 +1,21 @@
-import React, { useEffect, useRef } from 'react';
-import { WebView, WebViewMessageEvent } from 'react-native-webview';
-import { StyleSheet, View, StatusBar } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, StatusBar, View } from 'react-native';
 import Constants from 'expo-constants';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Auth = (): React.ReactElement => {
+export default function App(): React.ReactElement {
 	const statusBarHeight: number = Constants.statusBarHeight;
 	const webViewRef = useRef<WebView>(null);
+	const [isAuthPage, setIsAuthPage] = useState(false);
 
 	const handleWebViewLoad = () => {
 		webViewRef.current?.postMessage(statusBarHeight.toString());
+	};
+
+	const handleNavigationStateChange = (navState: any) => {
+		const currentUrl = navState.url;
+		setIsAuthPage(currentUrl.includes('/splash'));
 	};
 
 	useEffect(() => {
@@ -19,32 +26,40 @@ const Auth = (): React.ReactElement => {
 
 	return (
 		<>
-			<View style={styles.statusbar}>
-				<StatusBar
-					backgroundColor='transparent'
-					barStyle='dark-content'
-					translucent={true}
-				/>
-			</View>
-			<WebView
-				style={styles.container}
-				source={{ uri: `${process.env.EXPO_PUBLIC_CLIENT_URL}/auth` }}
-				ref={webViewRef}
-				onLoad={handleWebViewLoad}
+			<StatusBar
+				backgroundColor={isAuthPage ? 'transparent' : 'white'}
+				barStyle='dark-content'
+				translucent={isAuthPage}
 			/>
+			<SafeAreaView style={styles.safeArea}>
+				<View
+					style={[
+						styles.container,
+						{ marginTop: isAuthPage ? 0 : Constants.statusBarHeight },
+					]}
+				>
+					<WebView
+						style={styles.webView}
+						source={{ uri: 'https://harp-frontend.netlify.app/auth' }}
+						ref={webViewRef}
+						onLoad={handleWebViewLoad}
+						onNavigationStateChange={handleNavigationStateChange}
+					/>
+				</View>
+			</SafeAreaView>
 		</>
 	);
-};
+}
 
 const styles = StyleSheet.create({
+	safeArea: {
+		flex: 1,
+		backgroundColor: 'white',
+	},
 	container: {
 		flex: 1,
 	},
-	statusbar: {
-		backgroundColor: 'transparent',
+	webView: {
+		flex: 1,
 	},
 });
-
-export default Auth;
-
-//src/screens/auth/inde.tsx
