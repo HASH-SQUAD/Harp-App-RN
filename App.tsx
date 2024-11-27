78a875b32ac2d3bdd48314195d252ef758cd36e2
Jamkris <dltmdgus1412@gmail.com>
2024-11-27 19:39:20 +0900

    ADD :: 키보드 수정

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet, StatusBar, View } from 'react-native';
+import {
+	StyleSheet,
+	StatusBar,
+	View,
+	KeyboardAvoidingView,
+	Platform,
+	Keyboard,
+} from 'react-native';
 import Constants from 'expo-constants';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -26,6 +33,18 @@ export default function App(): React.ReactElement {
 		}
 	}, [statusBarHeight]);
 
+	useEffect(() => {
+		const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
+			webViewRef.current?.postMessage(
+				JSON.stringify({ type: 'SCROLL_BOTTOM' })
+			);
+		});
+
+		return () => {
+			showSubscription.remove();
+		};
+	}, []);
+
 	return (
 		<>
 			<StatusBar
@@ -40,15 +59,24 @@ export default function App(): React.ReactElement {
 						{ marginTop: isAuthPage ? 0 : Constants.statusBarHeight },
 					]}
 				>
-					<WebView
-						style={styles.webView}
-						source={{ uri: 'https://harp-frontend.netlify.app/splash' }}
-						// source={{ uri: 'http://localhost:3000/' }}
-						ref={webViewRef}
-						onLoad={handleWebViewLoad}
-						onNavigationStateChange={handleNavigationStateChange}
-						bounces={false}
-					/>
+					<KeyboardAvoidingView
+						behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+						keyboardVerticalOffset={0}
+						style={{ flex: 1 }}
+					>
+						<WebView
+							style={styles.webView}
+							source={{ uri: 'https://harp-frontend.netlify.app/splash' }}
+							ref={webViewRef}
+							bounces={false}
+							scrollEnabled={false}
+							onLoad={handleWebViewLoad}
+							onNavigationStateChange={handleNavigationStateChange}
+							onMessage={(event) => {
+								console.log('Message from WebView:', event.nativeEvent.data);
+							}}
+						/>
+					</KeyboardAvoidingView>
 				</View>
 			</SafeAreaView>
 		</>
